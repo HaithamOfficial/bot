1ac11abcb5e41f7e23a34a34072a9a8419f96bfb
Haitham Waheed <40340668+haithamwaheed@users.noreply.github.com>
2020-01-07 20:33:15 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$TOKEN = "1027323038:AAHGtM6qfgtWOkcJ_dG3tttTIi5GlZUjlDY"; // bot token
+$botUsername = "AntiMSbot"; // bot username without @
+
+/*
+$databaseHost = "localhost";
+$databaseName = ""; // db name
+$databaseUsername = ""; // db username
+$databasePassword = ""; // db password
+*/
+
+$sudoID = ""; // sudo id
+
+date_default_timezone_set("Asia/Riyadh");
